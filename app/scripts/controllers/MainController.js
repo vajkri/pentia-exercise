17a237180ad7edaa35e38cd55ea3419b49357a25
Krisztina Vajda <krisztavajda@gmail.com>
2016-02-24 23:17:19 +0100

    Add suggested JSON form data storage

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -4,6 +4,41 @@
 app.controller('MainCtrl', function ($scope) {
   $scope.user = {};
 
+  /* *
+  Suggested form data structure in JSON:
+  * */
+  /*
+  var registerFormData = [
+    {
+      sectionName: 'Tilmelding',
+      inputs: [
+        {
+          id: 'formRegisterName',
+          label: 'Navn',
+          labelClasses: '',
+          inputClasses: 'full-width',
+          type: 'text',
+          model: 'user.name',
+          placeholder: 'Navn *',
+          value: '',
+          required: true
+        },
+        {
+          id: 'formRegisterEmail',
+          label: 'E-mail',
+          labelClasses: '',
+          inputClasses: 'full-width',
+          type: 'text',
+          model: 'user.email',
+          placeholder: 'E-mail *',
+          value: '',
+          required: true
+        }
+      ]
+    }
+  ];
+  */
+
   $scope.register = function(user) {
     console.log('in function register()', user);
   }
