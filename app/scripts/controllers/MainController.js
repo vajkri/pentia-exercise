90d56f0f47760706708211dd507c0a015b9b3bbb
Krisztina Vajda <krisztavajda@gmail.com>
2016-02-24 22:47:45 +0100

    Add validation to register form

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -2,4 +2,9 @@
  * Created by krisztinavajda on 24/02/16.
  */
 app.controller('MainCtrl', function ($scope) {
+  $scope.user = {};
+
+  $scope.register = function(user) {
+    console.log('in function register()', user);
+  }
 });
